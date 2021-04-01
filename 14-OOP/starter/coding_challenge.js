a18d78a9f6d36bcc3295c603c57959e5eb54c408
Ji Zhang <zhangji0810@gmail.com>
2021-04-01 09:51:34 +0200

    constructor function

---
+++ 14-OOP/starter/coding_challenge.js
@@ -0,0 +1,57 @@
+"use strict";
+// Coding challenge #1
+/*const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`'${this.make}' going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`'${this.make}' going at ${this.speed} km/h`);
+};
+
+console.log(Car.prototype);
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+bmw.accelerate();
+mercedes.brake(); 
+
+// Coding challenge #2
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate = function () {
+    this.speed += 10;
+    console.log(`'${this.make}' going at ${this.speed} km/h`);
+  };
+
+  brake = function () {
+    this.speed -= 5;
+    console.log(`'${this.make}' going at ${this.speed} km/h`);
+  };
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford); */
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,107 @@
-'use strict';
+"use strict";
+
+/* const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const jonas = new Person("Jonas", 1991);
+const ji = new Person("Ji", 1986);
+console.log(jonas, ji);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, ji.species);
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+
+console.log(Person.prototype);
+console.dir(Person.prototype.constructor); 
+
+// const arr = [3, 5, 6, 5, 2, 1, 3, 2, 1];
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const PersonC1 = class {};
+
+class PersonC1 {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+const jessica = new PersonC1("Jessica", 1996);
+console.log(jessica);
+
+const account = {
+  owner: "jonas",
+  movements: [200, 530, 12, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set first(mov) {
+    this.movements.unshift(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.first = 50;
+console.log(account.movements);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = "Steven";
+steven.birthYear = "2008";
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init("Sarah", 1979);
+sarah.calcAge();*/
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student("Mike", 2020, "Computer Science");
+console.log(mike);
+mike.introduce();
+mike.calcAge();
