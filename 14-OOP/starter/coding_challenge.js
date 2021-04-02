f24f16b14f1eefc1f03b81c40fb2fb9579bb9f1b
Ji Zhang <zhangji0810@gmail.com>
2021-04-02 22:19:17 +0200

    finish ES6 class

--- 14-OOP/starter/coding_challenge.js
+++ 14-OOP/starter/coding_challenge.js
@@ -1,6 +1,6 @@
 "use strict";
 // Coding challenge #1
-/*const Car = function (make, speed) {
+const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 };
@@ -21,10 +21,10 @@ const bmw = new Car("BMW", 120);
 const mercedes = new Car("Mercedes", 95);
 
 bmw.accelerate();
-mercedes.brake(); 
+mercedes.brake();
 
 // Coding challenge #2
-class CarCl {
+/*class CarCl {
   constructor(make, speed) {
     this.make = make;
     this.speed = speed;
@@ -54,4 +54,30 @@ console.log(ford.speedUS);
 ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
-console.log(ford); */
+console.log(ford); 
+
+// Coding Challe #3
+const Electric = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+Electric.prototype = Object.create(Car.prototype);
+
+Electric.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+Electric.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `'${this.make}' going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new Electric("Tesla", 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate(); */
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -79,8 +79,7 @@ steven.calcAge();
 
 const sarah = Object.create(PersonProto);
 sarah.init("Sarah", 1979);
-sarah.calcAge();*/
-
+sarah.calcAge();
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -105,3 +104,140 @@ const mike = new Student("Mike", 2020, "Computer Science");
 console.log(mike);
 mike.introduce();
 mike.calcAge();
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  //   Static method
+  static hey() {
+    console.log("Hey there!");
+  }
+}
+
+class Student extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+}
+
+const martha = new Student("Martha Jones", 2012, "Computer Science");
+
+martha.introduce();
+martha.calcAge();
+
+class Account {
+  locale = navigator.language;
+
+  //   Private field
+  #movements = [];
+  #pin;
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account("Janas", "EUR", 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());*/
+
+class Student extends Person {
+  // extends表示Student 作为 Person的一个子class
+  university = "University of Lisbon";
+  #studyHours = 0; //用#设置Private 隐藏变量，class外无法访问
+  #course;
+  static numSbujects = 10; //用 static 创建只属于本class的变量或者函数method
+
+  constructor(fullName, birthYear, startYear, course) {
+    super(fullName, birthYear); //super 设置继承自父class的变量
+
+    this.startYear = startYear;
+
+    this.#course = course;
+  }
+
+  introduce() {
+    console.log(`I study ${this.#course} at ${this.university}`);
+  }
+
+  study(h) {
+    this.#makeCoffe();
+    this.#studyHours += h;
+  }
+
+  #makeCoffee() {
+    return "Here is a coffe for you.";
+  }
+
+  get testScore() {
+    return this._testScore;
+  }
+
+  set testScore(score) {
+    this._testScore = score <= 20 ? score : 0;
+  }
+
+  static printCurriculum() {
+    console.log(`There are ${this.numSbujects} subjects.`);
+  }
+}
+const student = new Student("Janas", 2037, 2020, "Medicine");
