6eb1bcfb1e5a692f3444fb902f6bf7984e74cbe2
Ji Zhang <zhangji0810@gmail.com>
2021-04-09 10:44:24 +0200

    middle of the forkify. The parcel package doesn't work. So, stop here

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,4 +1,11 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+import icons from "url:../img/icons.svg";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +18,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    renderSpinner(recipeContainer);
+
+    // 1） Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipes)
+);
